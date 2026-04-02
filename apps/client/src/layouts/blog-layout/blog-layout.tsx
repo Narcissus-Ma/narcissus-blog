9ad8ae3a943b6583a7115d32cc60675bfdd74ede
Narcissus-Ma <103873240+Narcissus-Ma@users.noreply.github.com>
2026-04-02 14:07:15 +0800

    feat: 添加背景粒子特效

--- apps/client/src/layouts/blog-layout/blog-layout.tsx
+++ apps/client/src/layouts/blog-layout/blog-layout.tsx
@@ -2,16 +2,20 @@ import { Outlet } from 'react-router-dom';
 
 import styles from './blog-layout.module.css';
 
+import { UniverseEffect } from '@/components/atoms/universe-effect/universe-effect';
 import { HeaderNav } from '@/components/organisms/header-nav/header-nav';
 import { useInitTheme } from '@/hooks/use-init-theme';
 import { useSiteBootstrap } from '@/hooks/use-site-bootstrap';
+import { useThemeStore } from '@/stores/theme-store';
 
 export function BlogLayout() {
   useInitTheme();
   useSiteBootstrap();
+  const theme = useThemeStore((state) => state.theme);
 
   return (
     <div className={styles.page}>
+      <UniverseEffect isDarkMode={theme === 'dark'} />
       <HeaderNav />
       <main className={styles.main}>
         <Outlet />
