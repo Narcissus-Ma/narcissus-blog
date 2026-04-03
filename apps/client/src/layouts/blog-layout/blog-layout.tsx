6062cec6dc60a4953ebf3d2bee92cebdd467b59f
Narcissus-Ma <103873240+Narcissus-Ma@users.noreply.github.com>
2026-04-03 15:53:14 +0800

    feat: 添加loading页

--- apps/client/src/layouts/blog-layout/blog-layout.tsx
+++ apps/client/src/layouts/blog-layout/blog-layout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import styles from './blog-layout.module.css';
 
 import { ClickEffect } from '@/components/atoms/click-effect/click-effect';
+import { Loading } from '@/components/atoms/loading/loading';
 import { UniverseEffect } from '@/components/atoms/universe-effect/universe-effect';
 import { HeaderNav } from '@/components/organisms/header-nav/header-nav';
 import { useInitTheme } from '@/hooks/use-init-theme';
@@ -16,6 +17,7 @@ export function BlogLayout() {
 
   return (
     <div className={styles.page}>
+      <Loading />
       <UniverseEffect isDarkMode={theme === 'dark'} />
       <ClickEffect type="fireworks" />
       <HeaderNav />
